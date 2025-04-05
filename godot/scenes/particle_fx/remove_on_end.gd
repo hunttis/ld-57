551e050ddec54aa411e2865b8e5a08c73c832b50
Ville Hunttis Nuutinen <hunttis@me.com>
2025-04-05 19:12:25 +0300

    One more particle effect

--- godot/scenes/particle_fx/remove_on_end.gd
+++ godot/scenes/particle_fx/remove_on_end.gd
@@ -1,7 +1,11 @@
 extends GPUParticles2D
 
+func _ready():
+	emitting = true
+	for child in get_children():
+		child.emitting = true
+
 func _process(delta: float) -> void:
-	
 	for child in get_children():
 		if !child.emitting:
 			child.queue_free()
