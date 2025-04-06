6d6a567eae47468cea9efa42c33c601500f01543
Ville Hunttis Nuutinen <hunttis@me.com>
2025-04-06 09:24:48 +0300

    Added events for fx

--- godot/scenes/components/global.gd
+++ godot/scenes/components/global.gd
@@ -5,3 +5,8 @@ enum TEAM {None, Player, Enemy, Neutral}
 signal level_cleared
 signal game_over
 signal game_restart
+
+signal create_bullet_hit_fx(coords: Vector2)
+signal create_player_hit_fx(coords: Vector2)
+signal create_enemy_death_fx(coords: Vector2)
+signal create_nondamaging_hit_fx(coords: Vector2)
