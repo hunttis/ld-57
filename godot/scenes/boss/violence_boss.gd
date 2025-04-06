577c7b3658c03a533b0d9c4583a734f3250fc780
Ville Hunttis Nuutinen <hunttis@me.com>
2025-04-06 13:04:35 +0300

    Level 3 boss

--- godot/scenes/boss/violence_boss.gd
+++ godot/scenes/boss/violence_boss.gd
@@ -4,18 +4,23 @@ extends Node2D
 @onready var weakpoint = $WeakPoint
 @onready var blocker = $BossBody/Blocker
 @onready var striker = $BossBody/Striker
-@onready var bodypath = $BodyPath
-@onready var bodypathfollower = $BodyPath/BodyPathFollower
-@onready var blockerpath = $BlockerPath
-@onready var blockerpathfollower = $BlockerPath/BlockerPathFollower
-@onready var strikerpath = $StrikerPath
-@onready var strikerpathfollower = $StrikerPath/Follower
+@onready var bodypath = $BossBody/BodyPath
+@onready var bodypathfollower = $BossBody/BodyPath/BodyPathFollower
+@onready var blockerpath = $BossBody/Blocker/BlockerPath
+@onready var blockerpathfollower = $BossBody/Blocker/BlockerPath/BlockerPathFollower
+@onready var strikeridlepath = $BossBody/Striker/StrikerIdlePath
+@onready var strikeridlepathfollower = $BossBody/Striker/StrikerIdlePath/Follower
+@onready var strikerstrikepath = $BossBody/Striker/StrikerStrikePath
+@onready var strikerstrikepathfollower = $BossBody/Striker/StrikerStrikePath/Follower
 
 @onready var body_origin = bossbody.position
 @onready var blocker_origin = blocker.position
 var body_progress = 0
 var blocker_progress = 0
 var striker_progress = 0
+var striker_loops = 0
+var striker_attack = false
+
 var movement: Tween
 
 func _ready():
@@ -33,7 +38,29 @@ func _process(delta: float) -> void:
 	blocker.position = blockerpathfollower.position
 	blocker.rotation = blockerpathfollower.rotation + 90
 
-	striker_progress += delta / 3
-	strikerpathfollower.progress_ratio = pingpong(striker_progress, 1)
-	striker.position = strikerpathfollower.position
+	
+	if striker_progress > 1 && !striker_attack:
+		print("One loop done!")
+		striker_loops += 1
+		striker_progress = 0
+	else:
+		strikeridlepathfollower.progress_ratio = wrap(striker_progress, 0, 1)
+		striker.position = strikeridlepathfollower.position
+
+	if striker_loops == 3 && !striker_attack:
+		print("Starting attack!")
+		striker_attack = true
+		striker_loops = 0
+		striker_progress = 0
+		
+	if striker_attack:
+		strikerstrikepathfollower.progress_ratio = pingpong(striker_progress, 1)
+		striker.position = strikerstrikepathfollower.position
+
+		if striker_progress > 2:
+			print("Stopping attack")
+			striker_progress = 0
+			striker_attack = false
+	
+	striker_progress += delta
 	
