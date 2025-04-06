21cc00c418ad83b0f55ebc97e0be6bb15714d7b7
Miika Kasnio <miika.kasnio@wunderdog.fi>
2025-04-06 19:46:43 +0300

    add boss hp indicator to greed

--- godot/scenes/boss/greed_boss.gd
+++ godot/scenes/boss/greed_boss.gd
@@ -11,7 +11,13 @@ func _process(delta: float) -> void:
 	else:
 		Global.create_boss_growl_fx.emit(global_position)
 		growl_cooldown = 5.0 + randf_range(1, 3)
-	
+	var hp = 0
+	for tooth in get_tree().get_nodes_in_group("Tooth"):
+		hp += tooth.hitpoints
+
+	Global.boss_health_change.emit('Greed', hp)
+
+
 	if get_tree().get_node_count_in_group("Tooth") == 0:
 		Global.create_enemy_death_fx.emit(global_position)
 		Global.create_boss_death_fx.emit(global_position)
