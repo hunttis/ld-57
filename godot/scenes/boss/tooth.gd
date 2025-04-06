25e614b71bc494a1ce4cddec53de81ff3b75a9b7
Juuso Elo-Rauta <juuso.elo-rauta@wunderdog.fi>
2025-04-06 21:10:10 +0300

    tweaks here and there and everywhere

--- godot/scenes/boss/tooth.gd
+++ godot/scenes/boss/tooth.gd
@@ -12,29 +12,32 @@ var player
 
 var fastest_speed = 0.1
 var slowest_speed = 2.5
-var speed_up_curve = 1.5
+var speed_up_curve = 1.05
+var spread_amount = 50
 
-var base_wait = 3.0
-var min_wait = 0.1
+
+var max_life: float
 
 func _ready():
 	global_origin = global_position
 	order = int(name.substr(5, 1))
-	cooldown = calculate_cooldown()
+	cooldown = 0.5 * order
 	super ()
+	max_life = float(8 * hitpoints)
 	
+func remaining_life():
+	return get_parent().get_tree().get_nodes_in_group("Tooth").reduce(func(acc, tooth): return tooth.hitpoints + acc, 0)
 
 func calculate_speed():
-	var tooth_count = float(get_parent().get_tree().get_nodes_in_group("Tooth").size())
-	var percentage = 1 - float((tooth_count / 8.0))
-	var curved = pow(percentage, speed_up_curve)
-	var speed = lerp(slowest_speed, fastest_speed, curved)
+	var hp = remaining_life()
+	var percentage = 1 - float((hp / max_life))
+	var speed = lerp(slowest_speed, fastest_speed, percentage)
 	return speed
 
 func calculate_cooldown():
-	var tooth_count = float(get_parent().get_tree().get_nodes_in_group("Tooth").size())
-	var max_wait = base_wait + tooth_count * float(order % 8) / 8.0
-	return lerp(max_wait, min_wait, 1 - (tooth_count / 8.0))
+	var hp = remaining_life()
+	var max_wait = 1 - 1 * (hp / max_life)
+	return lerp(max_wait, 0.0, hp / max_life)
 
 
 func _process(delta):
@@ -55,9 +58,11 @@ func _process(delta):
 
 		attack_happening = true
 		var tween: Tween = get_tree().create_tween()
-		tween.set_trans(Tween.TRANS_EXPO)
+		tween.set_trans(Tween.TRANS_SPRING)
+		tween.set_ease(Tween.EASE_IN)
 		var speed = calculate_speed()
 		tween.tween_property(self, "global_position", player.global_position, speed)
+		tween.set_trans(Tween.TRANS_EXPO)
 		tween.tween_property(self, "global_position", global_origin, speed)
 		tween.tween_property(self, "attack", false, 1)
 		tween.tween_property(self, "attack_happening", false, 1)
