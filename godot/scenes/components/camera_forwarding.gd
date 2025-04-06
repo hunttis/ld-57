03729369d9856122244bed69163d47df15b16486
LoHii <vertti.viitanen@gmail.com>
2025-04-06 16:08:37 +0300

    Boss camera

--- godot/scenes/components/camera_forwarding.gd
+++ godot/scenes/components/camera_forwarding.gd
@@ -3,10 +3,11 @@ extends RemoteTransform2D
 var _player: Player
 var _max_forward: float = 150;
 
+@export var only_right: bool = false
 
 func _ready():
 	_player = get_parent() as Player
 
 func _process(delta):
-	
-	position.x = lerp(position.x, _player.last_direction * _max_forward, 0.025)
+	var dir = 1 if only_right else _player.last_direction
+	position.x = lerp(position.x, dir * _max_forward, 0.025)
