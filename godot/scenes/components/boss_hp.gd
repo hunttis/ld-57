f8640ba59cc3a0a11b6a29094fe530ad1b365149
LoHii <vertti.viitanen@gmail.com>
2025-04-07 21:52:52 +0300

    Cosmetic for limbo boss

--- godot/scenes/components/boss_hp.gd
+++ godot/scenes/components/boss_hp.gd
@@ -9,3 +9,5 @@ extends CenterContainer
 func set_display(boss_name: String, boss_hp: int):
 	name_label.text = boss_name
 	hp_label.text = str(boss_hp)
+	if boss_name == "Limbo":
+		hp_label.text = str(boss_hp - 0.4)
